6477895a237620aa9e2882a87436c1ddb90b0542
ibrahimmbyrrm <ibrahim_bayram22@yahoo.com>
2023-06-16 14:55:38 +0300

    Initial Commit

---
+++ PokemonProject-VIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonProject-VIPER
+//
+//  Created by İbrahim Bayram on 16.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
